e10ac804424e5cd811e56a02aa62d96c9ebd4355
pelkeyuscg <kpelkey@chicagobooth.edu>
2023-02-25 15:14:27 +0000

    scaffold task

--- db/migrate/20230225144434_devise_create_users.rb
+++ db/migrate/20230225144434_devise_create_users.rb
@@ -3,6 +3,8 @@
 class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
+      enable_extension("citext")
+      
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
